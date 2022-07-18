7132a2ae7f9418417b2fbe013d1b2de65f77a9df
Neibo Augustine <neiboaugustine@gmail.com>
2022-07-18 16:19:14 +0100

    fix: add foreign key constraint to the inventories table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('restrict');
         });
     }
 
